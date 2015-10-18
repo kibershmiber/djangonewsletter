6d9383e46402f4b7f07ab413642ddbafe76390e2
Poremchuk Evgeniy <e.poremchuk@gmail.com>
2015-10-18 16:59:09 +0300

    Add a main model and tweak admin model

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import SingUp
+
+class SingUpAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "update", "timestamp"]
+    class Meta:
+        model = SingUp
+
+admin.site.register(SingUp, SingUpAdmin)
--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class SingUp(models.Model):
+    email = models.EmailField()
+    full_name = models.CharField(max_length = 120, blank = False, null = True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    update = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return str(self.email)
